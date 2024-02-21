e570323ea1d663bf1cf6a50509798d81e44595db
Satoriowo <valeriana4151@gmail.com>
2024-02-20 21:53:23 -0600

    Practica 4

--- Lab Apps Moviles p/script.js
+++ Lab Apps Moviles p/script.js
@@ -1,28 +1,28 @@
-var txt = document.getElementById("txt");
-var texto = "";
+var txt1 = document.getElementById("tipo-gasto");
+var txt2 = document.getElementById("monto");
 
-const buttom = document.querySelector("button");
 
-buttom.onclick = function () {
-    texto = document.getElementById("txt").value;
-    console.log(txt);
-    alert(texto);
+var articulo = " ";
+var monto = 0;
+var total = 0;
+
+var seccTicket = document.getElementById("seccTicket");
+var totParra = document.getElementById("totParra");
+
+function agregar(){
+    articulo = txt1.value;
+    monto = parseInt(txt2.value);
+
+    var newE = document.createElement('p');
+    newE.innerHTML = articulo + " $" + monto;
+
+    total += monto;
+
+    seccTicket.insertBefore(newE, totParra);
+    limpiar();
 }
 
 function limpiar(){
-    document.getElementById("txt").value = " ";
+    document.getElementById("tipo-gasto").value = " ";
+    document.getElementById("monto").value = " ";
 }
-
-function cambiarColorPagina(){
-    var simbolos, color;
-    simbolos = "0123456789ABCDEF"; // es hexagesimal 
-    color = "#";
-
-    for ( var i = 0; i<6; i++){
-        color = color + simbolos[Math.floor(Math.random() * 16)];
-    }
-
-    document.body.style.background = color;
-
-}
-
