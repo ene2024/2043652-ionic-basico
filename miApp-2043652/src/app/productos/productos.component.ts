5fa8c6e99e358b362035d6b7996286e24166b728
Satoriowo <valeriana4151@gmail.com>
2024-04-23 20:22:12 -0600

    Practica 10: Rutas

--- miApp-2043652/src/app/productos/productos.component.ts
+++ miApp-2043652/src/app/productos/productos.component.ts
@@ -6,17 +6,20 @@ import { ConsultaService } from '../consulta.service';
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.scss'],
 })
-
 export class ProductosComponent  implements OnInit {
 
   constructor(private consulta: ConsultaService) { }
-  productos: any[] = [];
-  
-  obtenerProductos(): void{    this.consulta.getProductos()    .subscribe((resp:
-    Object) => {      console.log(resp);      this.productos = resp as any[];  })};
-  
-  ngOnInit(): void { 
-    this.obtenerProductos()
+
+  ngOnInit() {
+    this.obtenerProductos();
   }
-  
+
+  productos: any = [];
+
+  obtenerProductos() : void {
+    this.consulta.getProductos().subscribe(res => {
+      this.productos = res;
+    })
+  }
+
 }
